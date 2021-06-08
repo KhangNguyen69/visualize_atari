a064955d16bf5816748adb9bbef155f90d914f6f
KhangNguyen69 <36027503+KhangNguyen69@users.noreply.github.com>
2021-06-08 20:27:36 +0700

    Add files via upload

--- visualize_atari_ex/rollout.py
+++ visualize_atari_ex/rollout.py
@@ -1,7 +1,9 @@
 # Visualizing and Understanding Atari Agents | Sam Greydanus | 2017 | MIT License
 
 from __future__ import print_function
-import warnings ; warnings.filterwarnings('ignore') # mute warnings, live dangerously ;)
+import warnings;
+
+warnings.filterwarnings('ignore')  # mute warnings, live dangerously ;)
 
 import torch
 import torch.nn as nn
@@ -13,25 +15,29 @@ import numpy as np
 from PIL import Image
 
 # prepro = lambda img: imresize(img[35:195].mean(2), (80,80)).astype(np.float32).reshape(1,80,80)/255.
-prepro = lambda img: np.array(Image.fromarray(img[35:195].mean(2))).resize(size=(80,80)).astype(np.float32).reshape(1,80,80)/255.
+# fix bug
+prepro = lambda img: np.array(Image.fromarray(img[35:195].mean(2)).resize((80, 80)))\
+                         .astype(np.float32).reshape(1, 80, 80) / 255.
 
-def rollout(model, env, max_ep_len=3e3, render=False):
+
+def rollout(model, env, max_ep_len=3, render=False):
     history = {'ins': [], 'logits': [], 'values': [], 'outs': [], 'hx': [], 'cx': []}
-    
-    state = torch.Tensor(prepro(env.reset())) # get first state
-    episode_length, epr, eploss, done  = 0, 0, 0, False # bookkeeping
+
+    state = torch.Tensor(prepro(env.reset()))  # get first state
+    episode_length, epr, eploss, done = 0, 0, 0, False  # bookkeeping
     hx, cx = Variable(torch.zeros(1, 256)), Variable(torch.zeros(1, 256))
 
     while not done and episode_length <= max_ep_len:
         episode_length += 1
-        value, logit, (hx, cx) = model((Variable(state.view(1,1,80,80)), (hx, cx)))
+        value, logit, (hx, cx) = model((Variable(state.view(1, 1, 80, 80)), (hx, cx)))
         hx, cx = Variable(hx.data), Variable(cx.data)
         prob = F.softmax(logit)
 
-        action = prob.max(1)[1].data # prob.multinomial().data[0] # 
+        action = prob.max(1)[1].data  # prob.multinomial().data[0] #
         obs, reward, done, expert_policy = env.step(action.numpy()[0])
         if render: env.render()
-        state = torch.Tensor(prepro(obs)) ; epr += reward
+        state = torch.Tensor(prepro(obs));
+        epr += reward
 
         # save info!
         history['ins'].append(obs)
